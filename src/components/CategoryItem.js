869fdfddbba798e81de2b68e55469dfade2e29df
Mohamed-Khaled-pro <mohamed.khaled.pro1@gmail.com>
2025-08-28 00:08:58 +0300

    last FRONT

--- src/components/CategoryItem.js
+++ src/components/CategoryItem.js
@@ -4,7 +4,7 @@ function CategoryItem({ name, image, onClick, selected }) {
       onClick={onClick}
       className="flex flex-col items-center cursor-pointer"
     >
-      <div className={ `hover:border-4 hover:border-primaryGreen hover:shadow-md hover:shadow-primaryGreen ${selected || null ? "scale-125 border-4 border-primaryGreen shadow-md shadow-primaryGreen" : ""}   w-16 h-16 sm:w-20 sm:h-20 bg-white  shadow-md rounded-full overflow-hidden flex items-center justify-center `}>
+      <div className={ `hover:border-4 hover:border-primaryGreen hover:shadow-md hover:shadow-primaryGreen ${selected || null ? "scale-110 border-4 border-primaryGreen shadow-sm shadow-primaryGreen" : ""}   w-16 h-16 sm:w-20 sm:h-20 bg-white  shadow-md rounded-full overflow-hidden flex items-center justify-center `}>
         <img
           src={image}
           alt={name}
@@ -12,7 +12,7 @@ function CategoryItem({ name, image, onClick, selected }) {
         />
       </div>
  <p
-        className={`text-sm mt-2 text-center font-medium capitalize ${
+        className={`text-sm mt-4 text-center font-medium capitalize ${
           selected ? "text-primaryGreen font-semibold" : ""
         }`}
       >
